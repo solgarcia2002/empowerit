475e568e896067cc610b8357c56186acfe65cfe4
Ivana <ivanakahlo@gmail.com>
2022-03-19 11:23:10 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './componentes/Header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h1>Hola!!!!!!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "/index.css";
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {I18nextProvider}from "react-i18next";
+import i18next from "i18next";
+
+import global_es from "./translations/es/global.json";
+import global_en from "./translations/en/global.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    es:{
+      global: global_es
+    },
+    en:{
+      global: global_en
+    }
+  }
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
