1350449348b0f34745b37acbd301310da56e9349
Rebeka Heredia <rebeka_hr@hotmail.com>
2022-04-21 20:46:01 -0300

    correcciones de contenedor about y services

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import NavbarComp from './componentes/Navbar';
 import Covers from "./componentes/Media/Covers";
 import './App.css';
 import About from './componentes/About/About';
-<<<<<<< Updated upstream
 import Services from './componentes/Services/Services';
-import ControlledCarousel from './componentes/Carousels/Carousels';
-=======
 import ControlledCarousel from './componentes/Carousel/Carousel';
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -19,10 +15,7 @@ function App() {
            <div>
              <Covers/>
              <About/>
-<<<<<<< Updated upstream
              <Services/>
-=======
->>>>>>> Stashed changes
              <ControlledCarousel/>
            </div>
      </div >
