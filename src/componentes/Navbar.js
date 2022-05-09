6f77489f3830610bd5e1968fb06da4e0a659ba7d
Ivana <ivanakahlo@gmail.com>
2022-05-09 16:59:47 -0300

    Merge branch 'main' of https://github.com/solgarcia2002/empowerit

--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container,Nav, Navbar, NavDropdown, Button } from "react-bootstrap"; 
+import { Container,Nav, Navbar, NavDropdown,  } from "react-bootstrap"; 
 import './Navbar.css';
 import DropdownB from './DropdownB';
 import {Outlet,Link,} from "react-router-dom";
