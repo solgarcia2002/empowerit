81a180b644a4fbb11bf9e4647b54a442e05ea0c9
Ivana <ivanakahlo@gmail.com>
2022-07-22 16:19:38 -0300

    cambio

--- src/componentes/Carousels/Carousels.js
+++ src/componentes/Carousels/Carousels.js
@@ -16,7 +16,7 @@ function ControlledCarousel() {
         <div className='slide-container'> 
           <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/clinify.png' title="Clinify Health - Chicago, US"/>
           <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/betway.png' title="Betway - UK"/>
-          <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/renault.png' title="Renault - AR"/>
+          <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/arqsaravia.jpg' title="Arq Magdalena Saravia- AR"/>
         </div>
       </Carousel.Item>
       <Carousel.Item className='diap2'>
@@ -31,6 +31,11 @@ function ControlledCarousel() {
           <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/remolquesmirage.png' title="Remolques Mirage - AR"/>
           <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/mosaicoslosamigos.png' title="Mosaicos Los Amigos - AR"/>
           <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/lipinski.png' title="Propiedades Lipinski - AR"/>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item className='diap4'>
+        <div className='slide-container'>
+          <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/renault.png' title="Renault - AR"/>
         </div>
       </Carousel.Item>
     </Carousel>
