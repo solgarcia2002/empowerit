2701f93e5fb9ee32a480975b7d2eaf95c3d84c12
sgarcia <sgarcia@athenaworks.io>
2022-08-23 14:23:48 -0300

    changing poster and texts for SEO

--- src/componentes/Media/Covers.js
+++ src/componentes/Media/Covers.js
@@ -15,7 +15,7 @@ const Covers = () => {
                    poster={Poster}/>
             <img className='.d-none .d-lg-none .d-md-none img' alt='' src={Poster}/>
             <h2 className={'h1'}>EMPOWERIT</h2>
-            <h1 className='d-none d-md-block .d-lg-none subtitle'>{t("serv.ball")} - {t("solutions.ball-3-title")} - {t("serv.ball3")}</h1>
+            <h1 className='subtitle'>{t("serv.ball")} - {t("solutions.ball-3-title")} - {t("serv.ball3")}</h1>
             <h2> {t("cover.text")} </h2>
             <button className='btn-main hover-filled-slide-up'>
                 <Link to='/our-job'>
