bc212c8b987562c7c94322a43a34bcb4ae3ffeae
sgarcia <sgarcia@athenaworks.io>
2022-08-24 20:50:42 -0300

    changing poster and texts for SEO

--- src/componentes/Pages/Trabajo.js
+++ src/componentes/Pages/Trabajo.js
@@ -1,27 +1,41 @@
 import React from 'react'
-import { useTranslation} from "react-i18next";
+import {useTranslation} from "react-i18next";
 import './Portada.css';
-import { Helmet } from 'react-helmet';
+import {Helmet} from 'react-helmet';
+import ClientCard from "../ClientsCard/ClientsCard";
 
 const Trabajo = () => {
-  const {t} = useTranslation (["global"]);
-    return (
-      <div className="cont-todo">
-        <Helmet>
-            <title>Empowerit | {t("nav-bar.our-job")}</title>
-            
-        </Helmet>
-        <div className='cont-img'>
-              
-          <img className='portada' alt='portada-trabajo' src='https://cdn.pixabay.com/photo/2021/09/08/05/38/communication-6605646__340.jpg'/>
-        </div>
-        <div className='cont-titulo1' >
-            <h2 className="titulo-portada text-center">{t("our-job.title")}</h2>
-        </div>
-        <div className="cont-texto">
-          <p className="texto">{t("our-job.text")}</p>
-        </div>
+  const {t} = useTranslation(["global"]);
+  return (
+    <div className="cont-todo">
+      <Helmet>
+        <title>Empowerit | {t("nav-bar.our-job")}</title>
+
+      </Helmet>
+      <div className='cont-titulo1'>
+        <h2 className="titulo-portada text-center">{t("our-job.title")}</h2>
+        <h3 className="texto">{t("our-job.text")}</h3>
       </div>
+      <div className="clients-container">
+        <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/clinify.png'
+                    title="Clinify Health - Chicago, US"/>
+        <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/betway.png' title="Betway - UK"/>
+        <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/arqSaravia.png'
+                    title="Arq Magdalena Saravia- AR"/>
+        <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/silco.png' title="SilcoPC - AR"/>
+        <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/desagoteslavictoria.png'
+                    title="Desagotes La Victoria - AR"/>
+        <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/imagenmasarquitectura.png'
+                    title="Imagen+Arquitectura - AR"/>
+        <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/remolquesmirage.png'
+                    title="Remolques Mirage - AR"/>
+        <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/mosaicoslosamigos.png'
+                    title="Mosaicos Los Amigos - AR"/>
+        <ClientCard image='https://empowerit-assets.s3.amazonaws.com/clients/lipinski.png'
+                    title="Propiedades Lipinski - AR"/> <ClientCard
+        image='https://empowerit-assets.s3.amazonaws.com/clients/renault.png' title="Renault - AR"/>
+      </div>
+    </div>
   )
 }
 
